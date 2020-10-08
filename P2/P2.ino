3c2a22e578b386ecb72af5daf81c8f521ee1cf51
Artemas J. Radik <ajradik@gmail.com>
2020-10-07 21:45:32 -0400

    Update P2.ino

--- P2/P2.ino
+++ P2/P2.ino
@@ -34,6 +34,15 @@ const int photoresistorMax = 923;
 
 const int ledPins [4] = {10, 11, 12, 13};
 
+const int distanceSensorPin = 7;
+const int distanceMin = 3;
+const int distanceMax = 330;
+
+const int topServoPin = 5;
+const int bottomServoPin = 4;
+const int servoAngleMin = 0;
+const int servoAngleMax = 180;
+
 class DCMotorControlledByLightLevel: public ScheduledProcess {
   
   class FlashingLEDs: public ScheduledProcess {
@@ -74,6 +83,60 @@ class DCMotorControlledByLightLevel: public ScheduledProcess {
       flashingLEDs.scheduledDelay(map(lightValueConstrainedForLinearity, photoresistorMin, photoresistorMax, maxLEDscheduledDelay, minLEDscheduledDelay));
       
       scheduledDelay(15);
+    }
+};
+
+class SynchronousOppositeServosByDistance : public ScheduledProcess {
+  
+  #include <Servo.h>
+  Servo topServo;
+  Servo bottomServo;
+  
+  public:
+    SynchronousOppositeServosByDistance() {
+      pinMode(distanceSensorPin, INPUT);
+      pinMode(topServoPin, OUTPUT);
+      pinMode(bottomServoPin, OUTPUT);
+      topServo.attach(topServoPin);
+      bottomServo.attach(bottomServoPin);
+    }
+  
+  	void scheduledLoop() override {
+      
+      int distance = distanceInCm();
+      int angle = map(constrain(distance, distanceMin, distanceMax), distanceMin, distanceMax, servoAngleMin, servoAngleMax);
+      
+      topServo.write(angle);
+      bottomServo.write(servoAngleMax - angle);
+      scheduledDelay(15);
+    }
+  	
+    long distanceInCm() {
+      //code citation https://www.tinkercad.com/things/9AzmS567fJN-ultrasonic-distance-sensor
+
+      // establish variables for duration of the ping,
+      // and the distance result in inches and centimeters:
+      long duration, cm;
+
+      // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+      // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+      pinMode(distanceSensorPin, OUTPUT);
+      digitalWrite(distanceSensorPin, LOW);
+      delayMicroseconds(2);
+      digitalWrite(distanceSensorPin, HIGH);
+      delayMicroseconds(5);
+      digitalWrite(distanceSensorPin, LOW);
+
+      // The same pin is used to read the signal from the PING))): a HIGH
+      // pulse whose duration is the time (in microseconds) from the sending
+      // of the ping to the reception of its echo off of an object.
+      pinMode(distanceSensorPin, INPUT);
+      duration = pulseIn(distanceSensorPin, HIGH);
+
+      // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+      // The ping travels out and back, so to find the distance of the
+      // object we take half of the distance travelled.
+      return duration / 29 / 2;
     }
 };
  
